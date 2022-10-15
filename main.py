12f8a17573535b5edd51241fe27b42af5020f29f
Supritha26 <115646725+SuprithaShalkeri@users.noreply.github.com>
2022-10-15 12:03:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+str1 = input("enter string 1: ")
+str2 = input("enter string 2: ")
+if(str1 !=""):
+    if(str1==str2):
+        print("string is equal")
+    else:
+        print("string is not equal")
+else:
+    print("please enter stringto check !!!!!1")
